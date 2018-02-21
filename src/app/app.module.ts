88ed1687132a28b1fac888d95b934f999b7ff010
txt_09 <txt_09@txts-mbp-09.lan>
2018-02-20 21:07:32 -0800

    Forms Challenge completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +19,9 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -7,10 +9,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public email: string;
+  public first: string;
+  public last: string;
+  public message: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public pepe: HttpClient) { }
 
   ngOnInit() {
+
+  }
+  onSubmit() {
+    const payload = {
+      sealName: 'Bryan',
+      email: this.email,
+      firstName: this.first,
+      lastName: this.last,
+      content: this.message,
+
+    };
+    console.log(payload)
+    this.pepe.post('https://seals-server.herokuapp.com' + '/contact-seals', payload)
+      .subscribe((res) => {
+        console.log(res)
+      }, (err) => {
+        console.log(err);
+      })
   }
 
 }
